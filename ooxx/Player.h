e39d65727ada570ab8c1ac9ee8bd0e81cbca67a1
maple0813 <127190552+maple0813@users.noreply.github.com>
2024-06-18 15:13:53 +0800

    Update Player.h

--- ooxx/Player.h
+++ ooxx/Player.h
@@ -1,32 +1,20 @@
-#ifndef P_H
-#define P_H
-#include"Card.h"
+#ifndef PLAYER_H
+#define PLAYER_H
 
-class Player
-{
-private:
-	Card* cards;
-	int cStyle;
-	bool win;
-	int money;
-	int stakeMoney;
-	bool inPlay;
+#include <string>
+using namespace std;
 
+class Player {
 public:
-	Player();
-	void setCards(Card*);
-	void setCstyle(int);
-	int getCstyle();
-	Card* getCards();
-	bool getWin();
-	void setWin(bool);
-	void setMoney(int);
-	int getMoney();
-	void setStakeMoney(int);
-	int getStakeMoney();
-	void setInPlay(bool);
-	bool getInPlay();
-	void winMoney();
-	void loseMoney();
+    Player(string name, char symbol);
+    string getName();
+    char getSymbol();
+    void setName(string name);
+    void setSymbol(char symbol);
+
+private:
+    string name;
+    char symbol;
 };
-#endif
+
+#endif // PLAYER_H
