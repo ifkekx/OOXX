33d262ff7dcb30739e5b7df8544099582a8e58c9
maple0813 <127190552+maple0813@users.noreply.github.com>
2024-06-18 15:13:32 +0800

    Update Player.cpp

--- ooxx/Player.cpp
+++ ooxx/Player.cpp
@@ -1,95 +1,24 @@
-﻿#include"Player.h"
+﻿#include<string>
+#include "player.h"
+using namespace std;
 
-Player::Player()
-{
-    this->cards = new Card[5];
-    win = 0;
-    money = 10000;
-    inPlay = true;
-}
-void Player::setCards(Card* cards)
-{
-    for (int i = 0; i < 5; ++i)
-        this->cards[i] = cards[i];
+Player::Player(string name, char symbol) {
+    this->name = name;
+    this->symbol = symbol;
 }
 
-void Player::setCstyle(int v)
-{
-    cStyle = v;
+string Player::getName() {
+    return name;
 }
 
-int Player::getCstyle()
-{
-    return cStyle;
+char Player::getSymbol() {
+    return symbol;
 }
 
-Card* Player::getCards()
-{
-    return cards;
+void Player::setName(string name) {
+    this->name = name;
 }
 
-bool Player::getWin()
-{
-    return win;
-}
-
-void Player::setWin(bool w)
-{
-    win = w;
-}
-
-void Player::setMoney(int money)
-{
-    this->money = money;
-}
-
-int Player::getMoney()
-{
-    return money;
-}
-
-void Player::setStakeMoney(int stakeMoney)
-{
-    this->stakeMoney = stakeMoney;
-}
-
-int Player::getStakeMoney()
-{
-    return stakeMoney;
-}
-
-void Player::setInPlay(bool value)
-{
-    inPlay = value;
-}
-
-bool Player::getInPlay()
-{
-    return inPlay;
-}
-
-void Player::winMoney()
-{
-    if (cStyle == 1)
-        money += stakeMoney;
-    else if (cStyle == 2)
-        money += stakeMoney * 2;
-    else if (cStyle == 3)
-        money += stakeMoney * 3;
-    else if (cStyle == 4)
-        money += stakeMoney * 5;
-}
-
-void Player::loseMoney()
-{
-    if (cStyle == 0)
-        money -= stakeMoney;
-    else if (cStyle == 1)
-        money -= stakeMoney * 2;
-    else if (cStyle == 2)
-        money -= stakeMoney * 3;
-    else if (cStyle == 3)
-        money -= stakeMoney * 4;
-    else if (cStyle == 4)
-        money -= stakeMoney * 6;
+void Player::setSymbol(char symbol) {
+    this->symbol = symbol;
 }
